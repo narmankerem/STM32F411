b69fc6b1c266a5d5da7228e625881e70606bc552
narmankerem <keremnrmn@gmail.com>
2024-08-15 11:38:33 +0300

    Continuous conversion with ADC

---
+++ ADC_Continuous_Conversion_Project9/Debug/Src/main.d
@@ -0,0 +1,18 @@
+Src/main.o: ../Src/main.c \
+ /home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/core_cm4.h \
+ /home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/cmsis_version.h \
+ /home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/cmsis_compiler.h \
+ /home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/cmsis_gcc.h \
+ /home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/mpu_armv7.h \
+ /home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/uart.h ../Inc/adc.h
+/home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/core_cm4.h:
+/home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/cmsis_version.h:
+/home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/cmsis_compiler.h:
+/home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/cmsis_gcc.h:
+/home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Include/mpu_armv7.h:
+/home/kerem/STM32CubeIDE/workspace_1.16.0/Chip_Header/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/uart.h:
+../Inc/adc.h:
